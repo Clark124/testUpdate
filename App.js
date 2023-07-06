d157ec120e028ff484c6dbc53f22f2737d269747
Clark124 <53799906@qq.com>
2023-07-06 14:51:32 +0800

    update

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>测试更新4444455</Text>
+      <Text>测试更新4444455666</Text>
       <StatusBar style="auto" />
     </View>
   );
